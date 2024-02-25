ba38e2b34cec77b409e108c22790cb7f2a91c3d9
Priyanshi7020 <80192384+Priyanshi7020@users.noreply.github.com>
2024-02-25 22:56:05 +0530

    Create Capital.h

---
+++ Capital.h
@@ -0,0 +1,16 @@
+#ifndef CAPITAL_H
+
+#define CAPITAL_H
+
+#include <iostream>
+#include "City.h"
+
+class Capital : public City {
+    //string state;
+public:
+    Capital(string name);
+    //string get_stateName();
+
+};
+
+#endif
