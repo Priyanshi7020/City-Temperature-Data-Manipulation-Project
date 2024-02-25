37d79ec58f0e43f11bf33ccdccb9e0d6c15a2f48
Priyanshi7020 <80192384+Priyanshi7020@users.noreply.github.com>
2024-02-25 22:57:06 +0530

    Create Menu.h

---
+++ Menu.h
@@ -0,0 +1,35 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include <iostream>
+#include "City.h"
+#include "Non_capital.h"
+#include "Capital.h"
+#include <vector>
+using namespace std;
+
+class Menu
+{
+    int choice;
+
+public:
+
+    Menu();
+    void display_menu();
+    void accept_choice();
+    void menu_case(vector<City*> cities);
+    void display_cities(vector<City*> cities);
+    void sort_choices(vector<City*> cities);
+    void convert_choices(vector<City*> cities);
+    void convert(vector<City*> cities, char unit);
+    void sort_cities(vector<City*> cities, int sort_choice);
+    void search_city(vector<City*> cities, string city_name);
+    void delete_city(vector<City*> cities, string city_name);
+};
+
+bool min_val(City* c1, City* c2);
+bool max_val(City* c1, City* c2);
+bool avg_val_ascending(City* c1, City* c2);
+
+
+#endif
