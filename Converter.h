b5ff4751c8a85f648f48951c64911debcf20a206
Priyanshi7020 <80192384+Priyanshi7020@users.noreply.github.com>
2024-02-25 22:57:47 +0530

    Update Converter.h

--- Converter.h
+++ Converter.h
@@ -1,1 +1,21 @@
+#ifndef CONVERTER_H
+#define CONVERTER_H
 
+#include "Menu.h"
+#include "City.h"
+#include "Capital.h"
+#include "Non_capital.h"
+#include<iostream>
+using namespace std;
+
+class Converter {
+	double multiplier;
+	double offset;
+
+public:
+	Converter();
+	Converter(const double multiplier, const double offset);
+	double converter(double num);
+};
+
+#endif // 
