4e2b15a65d5bf288146c528f16143f57c35f6173
Priyanshi7020 <80192384+Priyanshi7020@users.noreply.github.com>
2024-02-25 23:01:48 +0530

    Create Converter.cpp

---
+++ Converter.cpp
@@ -0,0 +1,25 @@
+#ifndef CONVERTER_CPP
+#define CONVERTER_CPP
+
+#include "Menu.h"
+#include "City.h"
+#include "Capital.h"
+#include "Non_capital.h"
+#include "Converter.h"
+#include<iostream>
+using namespace std;
+
+
+Converter::Converter() {
+	multiplier = 0;
+	offset = 0;
+	}
+Converter::Converter(const double multiplier, const double offset) {
+	this->multiplier = multiplier;
+	this->offset = offset;
+	}
+double Converter::converter(double num) {
+	return num * (double)multiplier + (double)offset;
+	}
+
+#endif // 
