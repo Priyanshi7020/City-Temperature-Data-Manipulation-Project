69f26e992ff54cb20447ec001c8a3dbaa7834d2f
Priyanshi7020 <80192384+Priyanshi7020@users.noreply.github.com>
2024-02-28 09:03:44 +0530

    Update Capital.cpp

--- Capital.cpp
+++ Capital.cpp
@@ -12,10 +12,17 @@ Capital::Capital(string name) : City(name) {
     for (int i = 0; i < size; i++) {
         pTemp[i] = new double[total_samples];
     }
-    //this->state = state;
 }
-//string Capital::get_stateName() {
-//    return this->state;
-//}
+Capital::Capital(string name, string state) : City(name) {
+    size = 4;
+    pTemp = new double* [size];
+    for (int i = 0; i < size; i++) {
+        pTemp[i] = new double[total_samples];
+    }
+    this->state = state;
+}
+string Capital::get_stateName() {
+    return this->state;
+}
 
 #endif
